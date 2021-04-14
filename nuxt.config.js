df5823f29abceac6f399efc887b86d4efd233a5f
sunder <alexander@darklight.by>
2021-04-14 19:26:19 +0300

    * Added static files, custom logo * Created login page, move to main page design prototype

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,23 +1,27 @@
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'travel-note',
+    title: 'Travel&Note - ' + process.env.npm_package_description,
     meta: [
       { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' }
+      { name: 'viewport', content: 'width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0' },
+      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' },
+      { 'http-equiv': 'X-UA-Compatible', content: 'ie=edge' }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
     ]
   },
-
+  loading: { color: '#874c73', height: '3px' },
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [
-  ],
-
+  styleResources: {
+    sass: [
+      '@/assets/sass/helpers/_variables.sass'
+    ]
+  },
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
+    { src: '~/plugins/vue-semantic-ui.js', ssr: true }
   ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
@@ -31,10 +35,11 @@ export default {
 
   // Modules: https://go.nuxtjs.dev/config-modules
   modules: [
-    // https://go.nuxtjs.dev/axios
     '@nuxtjs/axios',
-    // https://go.nuxtjs.dev/pwa
-    '@nuxtjs/pwa'
+    '@nuxtjs/pwa',
+    '@nuxtjs/auth',
+    '@nuxtjs/style-resources',
+    'semantic-ui-vue/nuxt'
   ],
 
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
@@ -50,4 +55,4 @@ export default {
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
   }
-}
+};
